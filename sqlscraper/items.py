a245f7a59fa52515353d8f3f7936abf2f57cd4cd
ericsoi <erick.soi@hotmail.com>
2023-08-11 00:35:00 +0300

    added data cleanup and duplicate removal

--- sqlscraper/items.py
+++ sqlscraper/items.py
@@ -8,5 +8,6 @@ import scrapy
 
 class SqlscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    price = scrapy.Field()
+    url = scrapy.Field()
--- sqlscraper/settings.py
+++ sqlscraper/settings.py
@@ -62,9 +62,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "sqlscraper.pipelines.SqlscraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+    #    "sqlscraper.pipelines.SqlscraperPipeline": 300,
+    "sqlscraper.pipelines.PriceToUSDPipeline": 1,
+    "sqlscraper.pipelines.DuplicatesPipeline": 2,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
